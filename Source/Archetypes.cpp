d3ef9c74f5f885199f56f41ae19d31bd36d78f57
Infin8Gamer1 <Jacob@mjhsoftware.com>
2018-12-12 14:35:20 -0800

    Removed levels and uneeded behaviors

--- Source/Archetypes.cpp
+++ Source/Archetypes.cpp
@@ -16,9 +16,7 @@
 #include "Sprite.h"
 #include "Animation.h"
 #include "Physics.h"
-#include "PlayerShip.h"
 #include "TimedDeath.h"
-#include "MonkeyMovement.h"
 #include "BulletMovement.h"
 #include "ColliderPoint.h"
 #include "ColliderCircle.h"
@@ -29,26 +27,6 @@
 #include "ColliderTilemap.h"
 #include "Enemy.h"
 #include <Graphics.h>
-
-GameObject * Archetypes::CreateShip(Mesh * mesh)
-{
-	GameObject* ship = new GameObject("Ship");
-	//transform
-	Transform* transform = new Transform(Vector2D(0, 0), Vector2D(50, 50), 0.0f);
-	ship->AddComponent(transform);
-	//Sprite
-	Sprite* sprite = new Sprite();
-	sprite->SetMesh(mesh);
-	ship->AddComponent(sprite);
-	//Physics
-	Physics* physics = new Physics();
-	ship->AddComponent(physics);
-	//PlayerShip
-	Behaviors::PlayerShip* ps = new Behaviors::PlayerShip();
-	ship->AddComponent(ps);
-
-	return ship;
-}
 
 GameObject * Archetypes::CreateBulletArchetype(Mesh * mesh)
 {
@@ -71,36 +49,6 @@ GameObject * Archetypes::CreateBulletArchetype(Mesh * mesh)
 	bullet->AddComponent(bm);
 
 	return bullet;
-}
-
-GameObject * Archetypes::CreateMonkey(Mesh * mesh, SpriteSource * spriteSource)
-{
-	GameObject* monkey = new GameObject("Monkey");
-	//transform
-	Transform* transform = new Transform(Vector2D(0, 0), Vector2D(100, 100), 0.0f);
-	monkey->AddComponent(transform);
-	//Sprite
-	Sprite* sprite = new Sprite();
-	sprite->SetMesh(mesh);
-	sprite->SetSpriteSource(spriteSource);
-	monkey->AddComponent(sprite);
-	//Animation
-	Animation* animation = new Animation();
-	//animation->Play(0, 8, 0.4f, false);
-	monkey->AddComponent(animation);
-	//Physics
-	Physics* physics = new Physics();
-	physics->SetMass(1.0f);
-	physics->SetGravity(Vector2D(0, -98.0f));
-	monkey->AddComponent(physics);
-	//Box Collider
-	ColliderRectangle* collider = new ColliderRectangle(Vector2D(abs(transform->GetScale().x / 2), abs(transform->GetScale().y / 2)));
-	monkey->AddComponent(collider);
-	//Monkey Movement
-	Behaviors::MonkeyMovement* mm = new Behaviors::MonkeyMovement();
-	monkey->AddComponent(mm);
-
-	return monkey;
 }
 
 GameObject * Archetypes::CreateCircle(Mesh * mesh, SpriteSource * spriteSource)
@@ -228,36 +176,6 @@ GameObject * Archetypes::CreatePlatformerTilemapObject(Mesh * mesh, SpriteSource
 	tileMap->AddComponent(colliderTilemap);
 
 	return tileMap;
-}
-
-GameObject * Archetypes::CreatePlatformerMonkey(Mesh * mesh, SpriteSource * spriteSource)
-{
-	GameObject* monkey = new GameObject("Monkey");
-	//transform
-	Transform* transform = new Transform(Vector2D(-800, 310), Vector2D(100, 100), 0.0f);
-	monkey->AddComponent(transform);
-	//Sprite
-	Sprite* sprite = new Sprite();
-	sprite->SetMesh(mesh);
-	sprite->SetSpriteSource(spriteSource);
-	monkey->AddComponent(sprite);
-	//Animation
-	Animation* animation = new Animation();
-	//animation->Play(0, 8, 0.4f, false);
-	monkey->AddComponent(animation);
-	//Physics
-	Physics* physics = new Physics();
-	physics->SetMass(1.0f);
-	physics->SetGravity(Vector2D(0, -98.0f));
-	monkey->AddComponent(physics);
-	//Box Collider
-	ColliderRectangle* collider = new ColliderRectangle(Vector2D(abs(transform->GetScale().x / 2), abs(transform->GetScale().y / 2)));
-	monkey->AddComponent(collider);
-	//Monkey Movement
-	Behaviors::MonkeyMovement* mm = new Behaviors::MonkeyMovement();
-	monkey->AddComponent(mm);
-
-	return monkey;
 }
 
 GameObject * Archetypes::CreateCollectable(Mesh * mesh, SpriteSource * spriteSource, Vector2D translation)
