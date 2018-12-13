3da7469737aba30adc4c29716a897e2167dbcd30
Infin8Gamer1 <Jacob@mjhsoftware.com>
2018-12-13 13:25:58 -0800

    Added Stuff to place tile function

--- Source/PlacePathTile.cpp
+++ Source/PlacePathTile.cpp
@@ -5,6 +5,7 @@
 #include <Vector2D.h>
 #include <ColliderTilemap.h>
 #include <GameObject.h>
+#include <Graphics.h>
 
 
 Behaviors::PlacePathTile::PlacePathTile() : Component("PlacePathTile")
@@ -23,8 +24,9 @@ void Behaviors::PlacePathTile::Initialize()
 void Behaviors::PlacePathTile::Update(float dt)
 {
 	UNREFERENCED_PARAMETER(dt);
-	if (Input::GetInstance().CheckReleased(' ')) {
-		SetTileToPath(Input::GetInstance().GetCursorPosition());
+	//see if the player has clicked and has enough tiles
+	if (Input::GetInstance().CheckTriggered(VK_LBUTTON)) {
+		SetTileToPath(Graphics::GetInstance().ScreenToWorldPosition(Input::GetInstance().GetCursorPosition()));
 	}
 }
 
@@ -38,5 +40,20 @@ void Behaviors::PlacePathTile::SetTileToPath(Vector2D MousePos)
 	ColliderTilemap* CT = static_cast<ColliderTilemap*>(GetOwner()->GetComponent("Collider"));
 	Vector2D tile = CT->ConvertWorldCordsToTileMapCords(MousePos);
 
-	map->SetCellValue(static_cast<int>(tile.x), static_cast<int>(tile.y), 1);
+	int tileX = static_cast<int>(tile.x);
+	int tileY = static_cast<int>(tile.y);
+
+	//if the chosen path tile is a path then return
+	if (map->GetCellValue(tileX, tileY) == 1) {
+		return;
+	}
+
+	//check if the chosen tile is neiboring another path
+	if (map->GetCellValue(tileX, tileY + 1) == 1 || map->GetCellValue(tileX, tileY - 1) == 1 || map->GetCellValue(tileX + 1, tileY) == 1 || map->GetCellValue(tileX - 1, tileY) == 1) {
+		map->SetCellValue(tileX, tileY, 1);
+
+		//remove one from the number of tiles the player can place
+	}
+
+	
 }
